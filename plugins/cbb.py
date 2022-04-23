b72e43b1e0ab3edab071a8ff09ac10e0dba85091
sbdipun9090 <100336442+sbdipun9090@users.noreply.github.com>
2022-04-24 02:19:41 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -11,7 +11,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Mᴀsᴛᴇʀ : <a href='https://t.me/COLD_ONEZ'>Cᴏʟᴅ々Oɴᴇᴢ࿐</a>\n○ Lᴀɴɢᴜᴀɢᴇ : <code>Python3</code>\n○ Lɪʙʀᴀʀʏ : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ : <a href='https://github.com/COLD-ONEZ/File-SHare-Bot'>Click Here</a>\n○ Cʜᴀɴɴᴇʟ : @Mo_Tech_YT\n○ Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ : @Mo_Tech_Group</b>",
+            text = f"<b>○ Mᴀsᴛᴇʀ : <a href='https://t.me/COLD_ONEZ'>Cᴏʟᴅ々Oɴᴇᴢ࿐</a>\n○ Lᴀɴɢᴜᴀɢᴇ : <code>Python3</code>\n○ Lɪʙʀᴀʀʏ : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ : <a href='https://github.com/COLD-ONEZ/File-SHare-Bot'>Click Here</a>\n○ Cʜᴀɴɴᴇʟ : @CineHub4U\n○ Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ : @CineHub4U_Group</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
